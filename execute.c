47d16e26a97cca26dfd14604e68d29b1a35aa649
ThatFireBoi <gcf2007@hotmail.com>
2023-09-08 04:44:49 +0000

    Implement swap opcode, added updated interpreter

--- execute.c
+++ execute.c
@@ -12,7 +12,10 @@
 int execute(char *content, stack_t **stack, unsigned int counter, FILE *file)
 {
 	instruction_t opst[] = {
-				{"push", f_push}, {"pall", f_pall}, {NULL, NULL}
+				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
+				{"pop",f_pop},
+				{"swap", f_swap},
+				{NULL, NULL}
 				};
 	unsigned int i = 0;
 	char *op;
